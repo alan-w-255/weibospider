4fc20965109c5ea1432fdc60a0b345209197374a
alan-w-255 <1649022341@qq.com>
2018-03-12 20:42:24 +0800

    update .gitignore file. 开始开发从百度百科中搜索地名和对存入mongodb的微博正文进行预处理

---
+++ baiduwiki/searchPlace.py
@@ -0,0 +1,15 @@
+import requests
+
+def GetBaiduWikiPage(placeName):
+    """
+    输入地名, 返回从百度百科上爬去到的网页
+    """
+    pass
+
+def ParseBaiduWikiPage(wikiPage):
+    """
+    输入百度百科网页, 返回从网页中获取的信息
+    """
+    pass
+
+
---
+++ preprocess_weibo_data/preprocess.py
@@ -0,0 +1,25 @@
+import pymongo
+
+def removeTags(weiboText):
+    """
+    输入微博正文, 输出去掉微博正文中链接以及其他一些标签后的文本
+    """
+    pass
+
+def extractPlaceNames(tagsFreeweiboText):
+    """
+    输入没有标签的微博正文, 返回微博正文中的地名
+    """
+    pass
+
+def findWeiboTextFromMongoDB(db):
+    """
+    输入数据库, 返回数据库中存储的微博正文
+    """
+def storePlaceNamesToMongoDB():
+    """
+    把微博正文中找到的标签存入mongodb.
+    """
+
+def main():
+    # todo: 
